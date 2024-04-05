36d3ef36676bc012a04263c3112cba29da9d5e5a
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-05 16:52:26 +0200

    feat: added logic and function

---
+++ jssnack7/js/functions.js
@@ -0,0 +1,14 @@
+
+/**
+ * Descrizione : creo una funzione per creare un array senza il primo elemento
+ * @param {array} arrayFunct
+ * @returns {array}
+ */
+
+function rimuoviDallaTesta(arrayFunct) {
+    let result = [];
+    for (i = 1; i < arrayFunct.length; i++) {
+        result.push(arrayFunct[i])
+    }
+    return result
+}
---
+++ jssnack7/js/script.js
@@ -0,0 +1,9 @@
+// scrivere una funzione rimuoviDallaTesta() che, dato un array A in input, restituisca un nuovo array B, con tutti gli elementi dell'array A, tranne quello in prima posizione
+
+
+// Creo l'array
+const array = ["1", "2", "3"]
+
+// Risultato finale
+const finalResult = rimuoviDallaTesta(array)
+console.log(finalResult);
